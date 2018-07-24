39130c4285d3fa42801f9ca604d22aba6beb0d8c
Achsah Abraham <achsahmp@gmail.com>
2018-07-24 16:03:40 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,14 +1,18 @@
 class Dog
-  def name=(name)
-    @name = name
+  def initialize=(dogs_name,dogs_breed = "Mutt")
+    @name = dogs_name
+    @breed = dogs_breed
+  end
+  def name=(dogs_name, dogs_breed = "Mutt")
+    @name = dogs_name
+    @breed = dogs_breed
   end
   def name
     @name
-  end
-  def breed=(breed)
-    @breed = breed
   end
   def breed
     @breed
   end
 end
+
+fido = Dog.new("Fido")
